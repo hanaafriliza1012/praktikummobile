7c58e933cb46b8329319d36899f547e6978e58b0
Hana Afriliza <hana.afrilizaa@gmail.com>
2020-10-13 09:21:57 +0700

    modul

---
+++ MyIntentApp/app/src/main/java/com/example/myintentapp/MoveActivity.java
@@ -0,0 +1,17 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MoveActivity extends AppCompatActivity {
+    private Button btnMovieActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
